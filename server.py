5893ecc03ae073602c0adccf781fe583084bf9d4
ksrb71 <ksrb71@outlook.com>
2020-05-15 08:32:14 +0530

    call Charge point function updated Main function written

--- server.py
+++ server.py
@@ -18,3 +18,36 @@ from ocpp.routing import on
 from ocpp.v16 import ChargePoint as cp
 from ocpp.v16.enums import *
 from ocpp.v16 import call_result, call
+
+
+class ChargePoint(cp) :
+    @on(Action.BootNotification)
+    def on_boot_notification ( self, charge_point_vendor, charge_point_model, **kwargs ) :
+        return call_result.BootNotificationPayload(
+            current_time=datetime.utcnow( ).isoformat( ),
+            interval=10,
+            status=RegistrationStatus.accepted
+        )
+
+    
+  sync def main():
+    server = await websockets.serve(
+        on_connect,
+        '0.0.0.0',
+        5603,
+        subprotocols=['ocpp1.6']
+    )
+    await server.wait_closed( )
+
+
+if __name__ == '__main__' :
+    try :
+        # asyncio.run() is used when running this example with Python 3.7 and
+        # higher.
+        asyncio.run(main( ))
+    except AttributeError :
+        # For Python 3.6 a bit more code is required to run the main() task on
+        # an event loop.
+        loop = asyncio.get_event_loop( )
+        loop.run_until_complete(main( ))
+        loop.close( )
